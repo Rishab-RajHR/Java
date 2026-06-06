6c56c55a6ef9d37d6a6706f0dad47c20747130a4
Rishab Raj <rr0948555@gmail.com>
2026-06-06 13:30:33 +0530

    Precdence and Associativity

---
+++ PreceAsso.java
@@ -0,0 +1,9 @@
+public class PreceAsso {
+     public static void main(String[] args) {
+         System.out.println("Precendence and Associativiy");
+        //  int result = 2 * 4/2;
+        //  int  result = 16 / 4 * 2;
+        int result = 2*2 + 4*4 - 2/2;
+         System.out.println(result);
+     }
+}
