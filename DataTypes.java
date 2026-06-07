a3d83ceb6cdde14da58b5ff45fb0e5b4115d45d5
Rishab Raj <rr0948555@gmail.com>
2026-06-07 22:50:40 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+     public static void main(String[] args) {
+           System.out.println("Data Types");
+          //  int a = 3;
+          //  int b = 4;
+          //  float b = 4.5f;
+          //  int result = a + b;
+          //  char b = 'c';  // unique code=>99
+          //  float result = a + b;
+          //  System.out.println(result);
+
+          // Increment and Decrement
+          // int a = 2;
+          // Pre Increment
+          // System.out.println(++a);
+          // Post Increment
+          // System.out.println(a++);
+          // System.out.println(a);
+
+          //  Pre Decrement
+          // System.out.println(--a);
+          // Post Decrement
+          // System.out.println(a--);
+          // System.out.println(a);
+
+          // char a = 'a';
+          char a = 'F';
+          System.out.println(++a);
+     }
+}
