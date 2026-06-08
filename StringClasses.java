0e6e14314918bf400428a4cb766741e8d0a3996d
Rishab Raj <rr0948555@gmail.com>
2026-06-08 20:28:06 +0530

    StringClasses

---
+++ StringClasses.java
@@ -0,0 +1,22 @@
+// import java.util.Scanner;
+
+public class StringClasses {
+      public static void main(String[] args) {
+
+            // Class
+            // String str = new String("Hello");
+            // System.out.println(str);
+
+            // Primitive data type
+            //  String str = "Hello World";
+            //  System.out.println(str);
+
+            // Scanner sc = new Scanner(System.in);
+            // String fullName = sc.nextLine();
+            // System.out.println(fullName);
+
+            int age = 22;
+            System.out.printf("Your Age is %d",age);
+
+      }
+}
