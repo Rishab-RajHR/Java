3893c34a2e30242ff0a806d279f05bde1a325121
Rishab Raj <rr0948555@gmail.com>
2026-06-10 18:33:53 +0530

    Strings

---
+++ Strings.java
@@ -0,0 +1,68 @@
+public class Strings {
+     public static void main(String[] args) {
+          String message = "Hi I'm Alex Pandian";
+          // String message = new String( "Hi I'm Alex Pandian");
+          System.out.println(message);
+
+          // length method
+          int stringLength = message.length();
+          System.out.println(stringLength);
+
+          // LowerCase Method
+          String lowerCase = message.toLowerCase();
+          System.out.println(lowerCase);
+
+          // UpperCase Method
+          String upperCase = message.toUpperCase();
+          System.out.println(upperCase);
+
+          // Trim method
+          String unTrimmedMessage ="    Alex    ";
+          System.out.println(unTrimmedMessage); 
+          String trimmedMessage = unTrimmedMessage.trim();
+          System.out.println(trimmedMessage);
+
+          // CharAt Method
+          // char charOfIndex = message.charAt(index:3);
+          // System.out.println(charOfIndex);
+
+
+
+          // IndexOf Method
+          // int indexOfCharacter = message.indexOf(ch:'r');
+          // int indexOfCharacter = message.indexOf('i',2);
+          // int indexOfCharacter = message.indexOf(str:"ar",fromIndex:2);
+          // System.out.println(indexOfCharacter);
+          
+
+          // Substring method
+          // String subStringMessage = message.substring(beginIndex:6);
+          // System.out.println(subStringMessage);
+
+
+          // Replace Method
+          // String replaceString = message.replace('n','R');
+          // String replaceString = message.replace('in','IN');
+          // System.out.println(replaceString);
+
+
+          // StartsWith Method
+          boolean startsString = message.startsWith("Hi");
+          System.out.println(startsString);
+
+          // StartsWith Method
+          boolean endsString = message.endsWith("an");
+          System.out.println(endsString);
+
+
+          // Equals Method
+          // String firstName = "Alex";
+          // boolean checkName = firstName.equals("Alex");
+          // System.out.println(checkName);
+
+          // Equals Method
+          String firstName = "Alex";
+          boolean checkName = firstName.equalsIgnoreCase("alex");
+          System.out.println(checkName);
+     }
+}
