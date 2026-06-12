3198d951609c6acf78e67661933020940ba97768
Rishab Raj <rr0948555@gmail.com>
2026-06-12 11:37:25 +0530

    Switch

---
+++ Switch.java
@@ -0,0 +1,21 @@
+public class Switch {
+    public static void main(String[] args) {
+        int age = 20;
+        switch (age) 
+        {
+          case 10:
+            System.out.println("Your Age is 10");
+            break;
+          case 20:
+            System.out.println("Your Age is 20");
+            break;
+     
+          case 30:
+            System.out.println("Your Age is 30");
+            break;
+
+          default:
+            System.out.println("Invalid Age");;
+        }
+    }
+}
