0aa93b1432d56e10e67bee8d1bcdda9e7030bb39
Rishab Raj <rr0948555@gmail.com>
2026-06-13 22:10:04 +0530

    Do While Loop

--- Loops.java
+++ Loops.java
@@ -10,7 +10,14 @@ public class Loops {
         //     a++;
         // }
 
-        
+        // Do While Loop
+        int a = 0;
+        do
+        {
+            System.out.println(a);
+            a++;
+        }
+        while(a<10);
 
     }
 }
