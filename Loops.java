d94762f85c8d787db7354a19332fa505d2bb482a
Rishab Raj <rr0948555@gmail.com>
2026-06-13 22:14:12 +0530

    For Loop

--- Loops.java
+++ Loops.java
@@ -11,13 +11,20 @@ public class Loops {
         // }
 
         // Do While Loop
-        int a = 0;
-        do
+        // int a = 0;
+        // do
+        // {
+        //     System.out.println(a);
+        //     a++;
+        // }
+        // while(a<10);
+
+
+        // For Loop
+        for(int i=0; i<10; i++)
         {
-            System.out.println(a);
-            a++;
+            System.out.println(i);
         }
-        while(a<10);
 
     }
 }
