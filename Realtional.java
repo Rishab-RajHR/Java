303b88c82ed245820f428c2d59562b6a2cbcdcda
Rishab Raj <rr0948555@gmail.com>
2026-07-12 15:05:01 +0530

    Relational Operators

---
+++ Realtional.java
@@ -0,0 +1,10 @@
+public class Realtional {
+     public static void main(String[] args) {
+          // Relational Operators. -> ==, != , <, >, <=, >=
+          int a = 5;
+          int b = 10;
+
+          boolean c = (a == b);
+          System.out.println(c);  // false
+     }
+}
