1a2ba1409f0bb6593184ee727e04f96ad06b58f9
Rishab Raj <rr0948555@gmail.com>
2026-07-14 17:35:35 +0530

    Loops

---
+++ loops2.java
@@ -0,0 +1,50 @@
+public class loops2 {
+      public static void main(String[] args) {
+          // Loops
+
+          // int i = 1;  // 1 to 10
+         
+
+          // System.out.println(i);
+          // i++;
+          // System.out.println(i);
+          // i++;
+          // System.out.println(i);
+          // i++;
+
+
+
+          //  int j = 1;
+
+          // // Loops  --> while  --> true/false
+          // while(j <= 10) {
+          //    System.out.println(j);
+          //    j++;
+          // }
+
+
+
+
+          // int i = 10;
+
+          // while(i >= 1){  // Loops  --> while  --> true/false
+          //       System.out.println(i);
+          //       i--;
+          // }
+
+
+
+          // DO-while loop
+
+          // do{
+          //     // do something
+          // }while(expression);
+
+
+          do{
+             
+          } while();
+
+
+      }
+}
