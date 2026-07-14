52505e7a99aff52e63993a5027feb875049d718a
Rishab Raj <rr0948555@gmail.com>
2026-07-14 14:46:26 +0530

    If Else if

--- ifelse.java
+++ ifelse.java
@@ -1,17 +1,88 @@
 public class ifelse {
      
     public static void main(String[] args) {
-          boolean b = true;
-          int i = 5;
+        
 
           // Selection statements
           // Normal if
-          if(i == 5){
-              System.out.println("i is 5");
-          }
-          else{
-              System.out.println("i is not 5");
-          }
+        //   if(i > 5){
+        //       System.out.println("i is greater than 5");
+        //   }
+        //   else{
+        //       System.out.println("i is less than or equal to 5");
+        //   }
+
+        // if(i % 2 == 0) {
+        //     System.out.println("i is even");
+        // }
+        // else{
+        //     System.out.println("i is odd");
+        // }
+
+
+        // Nested If
+        // if(i > 5){
+        //     if(i < 10) {
+                
+        //     }
+        //     else{
+
+        //     }
+        // }
+        // else{
+
+        // }
+
+
+        // Without nested if
+        // if(i > 5 && i < 10){
+
+        // }
+
+        // boolean b = true;
+        // int i = 9;
+
+        // // if-else-if ladder
+        // if (i == 5) {
+        //     System.out.println("i is 5");
+        // }
+        // else if(i == 6){
+        //     System.out.println("i is 6");
+        // }
+        // else if(i == 7){
+        //     System.out.println("i is 7");
+        // }
+        // else if(i == 8){
+        //     System.out.println("i is 8");
+        // }
+        // else if(i == 9){
+        //     System.out.println("i is 9");
+        // }
+        // else{
+        //      System.out.println("Number out of range");
+        // }
+
+
+        
+        // If-else if statement
+
+        int age = 50;
+
+        if(age > 80){
+            System.out.println("You are very old");
+        }
+        else if(age > 60){
+            System.out.println("You are old");
+        }
+        else if(age > 40){
+            System.out.println("You are becoming old");
+        }
+        else if(age > 20){
+            System.out.println("You are young");
+        }
+        else{
+            System.out.println("You are a child");
+        }
 
          
     }
