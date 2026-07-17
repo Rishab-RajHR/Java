22e05d9d3355dc1c14f9d6adc5ccb6369733db04
Rishab Raj <rr0948555@gmail.com>
2026-07-17 18:11:03 +0530

    Arrays

---
+++ arrays.java
@@ -0,0 +1,32 @@
+public class arrays {
+     public static void main(String[] args) {
+          int[] rollNums = new int[3];
+
+          // int rollNums[] = new int[3];  => It is also correct approach 
+
+          // 101, 102, 103
+
+          // rollNums[0] = 101;
+          // rollNums[1] = 102;
+          // rollNums[2] = 103;
+
+          // System.out.println(rollNums[0]);
+          // System.out.println(rollNums[1]);
+          // System.out.println(rollNums[2]);
+
+          // System.out.println(rollNums.length);
+
+
+          // With the help of loops
+          int x = 101;
+          for(int i=0; i<rollNums.length; i++) {
+              rollNums[i] = x;
+              x++;
+          }
+
+          for(int i=0; i<rollNums.length; i++) {
+              System.out.println(rollNums[i]);
+          }
+
+     }
+}
