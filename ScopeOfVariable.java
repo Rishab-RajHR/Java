21c71fdd0e1edd7c5c7934625ed969f70fd69a70
Rishab Raj <rr0948555@gmail.com>
2026-07-19 16:52:24 +0530

    Scope

---
+++ ScopeOfVariable.java
@@ -0,0 +1,28 @@
+public class ScopeOfVariable {
+
+    static String name = "Alex";  // global scope
+
+    public static void main(String[] args) {
+        // Scope of a variable
+
+         int x = 4;  // local scope
+         int y = 5;
+
+         if(x == 4){
+            int j = 7;
+            System.out.println(j);
+         }
+
+         System.out.println(x + " , " + y);
+
+         fun();
+    }
+
+    static void fun() {
+       int x = 4; // local scope
+       int y = 5;
+       System.out.println(x + " , " + y);
+
+       System.out.println(name);
+    }
+}
