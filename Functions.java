e22ebe1150d97b9b9283666ed476f3411e8b6ecf
Rishab Raj <rr0948555@gmail.com>
2026-07-19 13:25:30 +0530

    Functions

---
+++ Functions.java
@@ -0,0 +1,40 @@
+public class Functions {
+    public static void main(String[] args) {
+        // Functions in Java
+         
+        greet();
+
+        sayHello("Aditya"); // Arguments
+
+        int x = getNumber();
+        System.out.println(x);
+
+        System.out.println(multiply(12, 10));
+
+        return; // optional
+
+    }
+
+    // No Ip, No Op
+    static void greet() {
+       System.out.println("Hello");
+      //  return;
+    }
+
+    // There is Input, but No Output
+    static void sayHello(String name) {  // No of parameter
+        System.out.println("Hello " + name);
+    }
+
+    // No Input, But there is Output
+    static int getNumber() {
+       return 10;
+    }
+
+    // There is Input and Output
+    static int multiply(int a, int b) {
+        int mul =  a * b;
+        return mul;
+        // return (a * b);
+    }
+}
