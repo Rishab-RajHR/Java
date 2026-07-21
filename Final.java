66e6171d18d7142297c1749b6b685d2e3b2b96f6
Rishab Raj <rr0948555@gmail.com>
2026-07-21 17:32:46 +0530

    Final

---
+++ Final.java
@@ -0,0 +1,29 @@
+public class Final {
+    public static void main(String[] args) {
+         Random r1 = new Random();
+         System.out.println(r1.PI);
+
+        //  r1.PI = 3.16; => Cannot be reassigned
+
+        final int x = 4;
+        System.out.println(x);
+    }
+}
+
+class Random {
+   final double PI;
+
+   Random() {
+      this.PI = 3.14;
+   }
+
+  //  static keyword
+  class Random {
+     static final double PI;
+
+     static {
+       PI = 3.14;
+     }
+  }
+
+}
