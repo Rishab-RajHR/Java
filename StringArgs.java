7a3342e25acda531bb95a5f3cff22edbe56f9f24
Rishab Raj <rr0948555@gmail.com>
2026-07-21 17:35:14 +0530

    String Args

---
+++ StringArgs.java
@@ -0,0 +1,11 @@
+// Use of String[] args
+
+public class StringArgs {
+    public static void main(String[] args) {
+        System.out.println("Number of arguments are " + args.length);
+
+        for(int i=0; i<args.length; i++) {
+            System.out.println("Argument " + i + " = " + args[i]);
+        }
+    }
+}
