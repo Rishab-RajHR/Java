181bc3d7cf7713fea852853f83b9c1a025e361a1
Rishab Raj <rr0948555@gmail.com>
2026-07-23 12:20:00 +0530

    Abstraction

---
+++ abstract33.java
@@ -0,0 +1,44 @@
+public class abstract33 {
+     public static void main(String[] args) {
+        Car car = new ElectricCar();
+        car.start();
+        car.accelerate();
+        car.brake();
+     }
+}
+
+abstract class Car {
+    void start() {
+       System.out.println("Car Started");
+    }
+
+    abstract void accelerate();
+
+    abstract void brake();
+}
+
+class FuelCar extends Car {
+     
+  @Override
+  void accelerate() {
+    System.out.println("Fuel Car is accelerating");
+  }
+
+  @Override
+  void brake() {
+     System.out.println("Fuel Car is Stopping");
+  }
+}
+
+class ElectricCar extends Car {
+     
+  @Override
+  void accelerate() {
+       System.out.println("Electric Car is accelerating");
+  }
+
+  @Override
+  void brake() {
+       System.out.println("Electric Car is Stopping");
+  }
+}
