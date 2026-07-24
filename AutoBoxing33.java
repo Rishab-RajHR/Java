ec2e1b3b5dc4c6716c653f9971f1631204a6e824
Rishab Raj <rr0948555@gmail.com>
2026-07-24 10:27:59 +0530

    Auto Boxing

---
+++ AutoBoxing33.java
@@ -0,0 +1,15 @@
+public class AutoBoxing33 {
+     public static void main(String[] args) {
+      
+     }
+}
+
+class Sample {}
+
+class Sample2 {}
+
+// 1. Why one public class per file.
+// 2. Why name of that public class should be same as file name
+
+
+// JVM --> AutoBoxing.java.  --> AutoBoxing.main()
