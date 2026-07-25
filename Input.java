827dfd23d7eab9f84a644516e66266148fc7578d
Rishab Raj <rr0948555@gmail.com>
2026-07-25 22:57:09 +0530

    Input Stream

---
+++ Input.java
@@ -0,0 +1,8 @@
+public class Input {
+     public static void main(String[] args) {
+         System.out.println("Hello");
+     }
+}
+
+// System class --> PrintStream out
+// PrintStream --> println()
