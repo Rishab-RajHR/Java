8b3a92dbb9e1580959df5e40825274c23b115415
Rishab Raj <rr0948555@gmail.com>
2026-07-30 22:45:51 +0530

    All String

--- AllString.java
+++ AllString.java
@@ -7,7 +7,29 @@ public class AllString {
        String s3 = "Alex";
        String s4 = new String(s3);
 
-       System.out.println(s2);
+      //  System.out.println(s2);
+
+      // char array
+      char[] arr = {'A', 'l', 'e', 'x', ' ', 'K', 'u', 'm', 'a', 'r'};
+      String s5 = new String(arr);
+      // arr[0] = 'B';
+
+      // System.out.println(s5);
+
+      // Character array Subset
+      String s6 = new String(arr, 0, 6);
+      System.out.println(s6);
+
+      byte[] arr2 = {97, 98, 99};
+      String s7 = new String(arr2, 0, 2);
+
+      // System.out.println(s7);
+
+      // String Buffer / String Builder
+      StringBuffer sb = new StringBuffer("Hello");
+      String s8 = new String(sb);
+
+      System.out.println(s8);
     }
 }
 
