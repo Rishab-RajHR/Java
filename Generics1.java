1c0d0bf6b58e36d0b1ec8f071be76688ddd77263
Rishab Raj <rr0948555@gmail.com>
2026-07-31 21:56:22 +0530

    Generics

---
+++ Generics1.java
@@ -0,0 +1,23 @@
+public class Generics1 {
+    public static void main(String[] args) {
+        // Upcasting
+        String s = "Hello";
+        Object obj = s;
+
+        System.out.println(obj);
+
+        // Downcasting
+        Object obj2 = "Alex";
+        String s2 = (String) obj2;
+
+        System.out.println(s2);
+
+
+        // AutoBoxing
+
+        Object obj3 = 10;
+        String s3 = (String) obj3;
+
+        System.out.println(s3);   // Class Exception
+    }
+}
