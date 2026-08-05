91d715e4763b0af5ea39b0af284f15cd2cb6f8c6
Rishab Raj <rr0948555@gmail.com>
2026-08-05 22:03:37 +0530

    List Interface

---
+++ List1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class List1 {
+    public static void main(String[] args) {
+       List<Integer> list = new ArrayList<>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+
+       System.out.println(list.get(1));
+
+       list.set(1, 5);
+
+      //  System.out.println(list);
+
+       list.addAll(0, List.of(9,8,7));
+
+      //  System.out.println(list);
+
+       list.remove(0);
+
+      //  System.out.println(list);
+
+      list.indexOf(2);
+      list.lastIndexOf(5);
+
+      // ListIterator<Integer> it = list.listIterator(4);
+
+      // while(it.hasPrevious()) {
+      //     System.out.println(it.previous());
+      //     it.previous();
+      // }
+
+      List<Integer> l = List.of(1,2,3,4,5,6,7,8);
+
+      List<Integer> l2 = List.copyOf(l);
+      l2.add(7);
+      System.out.println(l);
+
+    }
+}
