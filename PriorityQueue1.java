429200e0dd3794e8fa3de05cae8e7006430c3599
Rishab Raj <rr0948555@gmail.com>
2026-08-11 15:54:42 +0530

    Priority Queue

---
+++ PriorityQueue1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class PriorityQueue1 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(10);
+        pq.offer(20);
+        pq.offer(30);
+        pq.offer(40);
+        pq.offer(50);
+
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+        // max heap
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>((a,b) -> b-a);
+        pq2.offer(10);
+        pq2.offer(20);
+        pq2.offer(30);
+        pq2.offer(40);
+        pq2.offer(50);
+
+        System.out.println(pq2.poll());
+        System.out.println(pq2.poll());
+        System.out.println(pq2.poll());
+        System.out.println(pq2.poll());
+        System.out.println(pq2.poll());
+    }
+};
+
