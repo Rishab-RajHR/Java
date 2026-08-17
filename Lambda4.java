22157656044a5026fd92dc5db66bef99dcb0cb09
Rishab Raj <rr0948555@gmail.com>
2026-08-17 22:49:20 +0530

    Lambda

--- Lambda4.java
+++ Lambda4.java
@@ -7,12 +7,22 @@ public class Lambda4 {
 
         // Function<Integer, Integer> equation = x -> ((x+2) * 3);
 
-        Function<Integer, Integer> add2 =  x -> x + 2;
-        Function<Integer, Integer> multipy3 = x -> x * 3;
+        Function<Integer, Integer> add2 =  x -> x + 2;  // f(x)
+        Function<Integer, Integer> multipy3 = x -> x * 3; // g(x)
 
-        int a = add2.apply(2);
-        int b = multipy3.apply(a);
+        // int a = add2.apply(2);
+        // int b = multipy3.apply(a);
 
-        System.out.println(b);
+        int ans = multipy3.apply(add2.apply(2));
+        // g(f(x))
+
+        int ans2 = add2.andThen(multipy3).apply(2);
+
+        Function<Integer, Integer> ansFunc = add2.andThen(multipy3);
+        int ans3 = ansFunc.apply(2);
+
+        System.out.println(ans3);
+
+        System.out.println(ans);
     }
 }
