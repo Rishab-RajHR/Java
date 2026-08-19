fc1aefb2450361eb26abfe914a49022048ea7136
Rishab Raj <rr0948555@gmail.com>
2026-08-19 18:58:25 +0530

    Streams

---
+++ Stream2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Stream2 {
+    public static void main(String[] args) {
+       List<Integer> list = new ArrayList<>(List.of(5, 12, 7, 14));
+
+      //  Stream<Integer> s = list.stream();
+      //  s = s.filter(x -> x > 10);
+      //  s = s.map(x -> x % 3);
+      //  s.forEach(System.out::println);
+
+      list.stream()
+          .filter(x -> x > 10)
+          .map(x -> x%2)
+          .forEach(System.out::println);
+    }
+}
