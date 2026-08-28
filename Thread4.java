55c2f38dab9a29524b177c3fd61485da95da2fdd
Rishab Raj <rr0948555@gmail.com>
2026-08-28 17:28:17 +0530

    Thread

--- Thread4.java
+++ Thread4.java
@@ -2,6 +2,14 @@ public class Thread4 {
       public static void main(String[] args) {
           System.out.println(Thread.currentThread().getName());
           System.out.println(Thread.currentThread().getId());
+
+
+          Thread t1 = new Thread(() -> {
+              System.out.println("Name of my thread is " + Thread.currentThread().getName());
+              System.out.println("Id of my thread is " + Thread.currentThread().getId());
+          });
+
+          t1.start();
       }
 }
 
