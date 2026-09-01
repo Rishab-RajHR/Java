8da6f4080b37878f2e5252d3e95f0b3543d4cf26
Rishab Raj <rr0948555@gmail.com>
2026-09-01 14:29:50 +0530

    InterThread

--- InterThread.java
+++ InterThread.java
@@ -2,9 +2,26 @@ public class InterThread {
     public static void main(String[] args) {
        Box box = new Box();
 
-       Thread t1 = new Thread(() -> box.producer(10));
+       Thread t1 = new Thread(() -> {
+            for(int i=1; i<=20; i++) {
+             box.producer(i);
+            //  try {
+            //     Thread.sleep(100);
+            //  } catch (NullPointerException e) {
+            //  }
+            }
+       });
+      
 
-       Thread t2 = new Thread(() -> box.consumer());
+       Thread t2 = new Thread(() -> {
+            for(int i=1; i<=20; i++) {
+             box.consumer();
+              // try {
+            //     Thread.sleep(70);
+            //  } catch (NullPointerException e) {
+            //  }
+            }
+       });
 
        t1.start();
        t2.start();
@@ -13,16 +30,26 @@ public class InterThread {
 }
 
 class Box {
-    Integer item;
-    Boolean flag = false;
+    volatile Integer item;
+    volatile Boolean flag = false;
 
     void producer(int value) {
+
+       while(flag == true) {
+        // do nothing
+       }
+
        item = value;
        flag = true;
        System.out.println("Producer produces " + item);
     }
 
     void consumer() {
+
+         while(flag == false) {
+        // do nothing
+        }
+
        System.out.println("Consumer consumes " + item);
        item = null;
        flag = false;
